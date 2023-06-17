97be82d91d3211572a6ff8eab6146e9b957b8f3c
Anas-Wael <anaswael600@gmail.com>
2023-06-18 00:57:53 +0200

    revised array functions

--- array_methods.php
+++ array_methods.php
@@ -1,75 +1,193 @@
 <?php
-$a = $b = array(
-  array(
-    'id' => 5698,
-    'first_name' => 'Peter',
-    'lastname' => 'Griffin',
-  ),
-  array(
-    'id' => 4767,
-    'first_name' => 'Ben',
-    'lastname' => 'Smith',
-  ),
-  array(
-    'id' => 3809,
-    'first_name' => 'Joe',
-    'lastname' => 'Doe',
-  )
+
+$a = array(
+    [
+        "id" => "50",
+        "first_name" => "anas",
+        "last_name" => "wael"
+    ],
+    [
+        "id" => "51",
+        "first_name" => "peter",
+        "last_name" => "parker"
+    ],
+    [
+        "id" => "52",
+        "first_name" => "roger",
+        "last_name" => "paterson"
+    ]
 );
 
-$user_info = array_column($a, 'lastname');                                        // array_column
-print_r($user_info);
-echo "<br>";
-print_r(array_diff($a[0], $a[1]));                                                // array_diff
-echo "<br>";
-print_r($a);
-$a = array_fill(0, 6, "anas is handsome");                                        // array_fill
-print_r($a);
-echo "<br>";
+$Ids = array_column($a, "first_name", "id");
+print_r($Ids);
 
-print_r(array_rand($user_info, 2));                                               // array_rand
-echo "<br>";
-$array = array(5, 12 , 1 ,2);
-echo array_product($array);                                                       // array_product
-echo "<br>";
-array_pop($a);                                                                    // array_pop
-print_r($a);
-echo "<br>";  
-$a =  array_pad($a, count($a) + 2, "anas is genious");                            // array_pad   
-print_r($a);
-echo "<br>";
-array_push($a, "Anas is adorable");                                               // array_push
-print_r($a);
-echo "<br>";
-print_r(array_rand($a, 3));
-echo "<br>";
-array_splice($user_info, 0, 1, ["anas", "roger"]);                                // array_splice  
-print_r($user_info);
-echo array_search("Smith", $user_info);                                           // array_search
-echo "<br>";
-$new_user_info = array_column($b, "id", "first_name");                            
-array_reverse($new_user_info);                                                    // array_reverse
-print_r($new_user_info);
-echo "<br>";
-array_shift($new_user_info);                                                      // array_shift
-print_r($new_user_info);
-$new_user_info = array_flip($new_user_info);                                      // array_flip
-array_pop($new_user_info);
-echo "<br>";
-print_r($new_user_info);
-echo "<br>";
-$new_user_info = array_pad($new_user_info, -2-count($new_user_info), "anas");
-print_r($new_user_info);
-echo "<br>";
-print_r(array_slice($new_user_info, 1, 1));      
-echo "<br>";                                                       
-$array0 = array("anas", "roger", "christina", "bob");
-print_r(array_slice($array0, 1, 3));                                              // array_slice
-echo "<br>";
-echo current($array0);                                                            // currenct 
-echo "<br>";
-echo next($array0);                                                               // next
-echo "<br>";
-echo prev($array0);                                                               // prev
 echo "<br>\n";
-echo "Hello, world";
+
+echo sizeof($a, 1);
+
+echo "<br>\n";
+
+$arr1 = ["Peter", "roger", "Andro"];
+$arr2 = ["is nice", "is old", "is nice"];
+
+$combination = array_combine($arr1, $arr2);
+
+print_r($combination);
+
+echo "<br>\n";
+
+$array = array_change_key_case($combination, CASE_UPPER);
+print_r($array);
+
+echo "<br>\n";
+
+$array = array_merge($arr1, $arr2);
+
+$array = array_chunk($array, 2);
+
+print_r($array);
+
+echo "<br>\n";
+
+$arr2 = ["Peter", "roger", "Peter", "anas", "anas", "Peter"];
+
+print_r(array_count_values($arr2));
+
+echo "<br>\n";
+
+print_r(array_fill(0, 3, "anas is handsome"));
+
+echo "<br>\n";
+
+print_r(array_fill_keys($arr2, "is handsome"));
+
+echo "<br>\n";
+
+print_r(array_flip($a[0]));
+
+echo "<br>\n";
+
+print_r(array_intersect(["peter", "roger", "anas", "andro"], ["roger", "andro", "banana"]));
+
+echo "<br>\n";
+
+$array = array_filter(["Peter", "roger", "Peter", "anas", "anas", "Peter"], function ($var) { if ($var !== "Peter") { return true; } });
+
+print_r($array);
+
+echo "<br>\n";
+
+if (array_key_exists("roger", $combination))
+{
+    echo "Roger is in the array";
+}
+
+echo "<br>\n";
+
+$arr = array_keys($combination);
+
+foreach($arr as $value)
+{
+    echo $value . " ";
+}
+
+echo "<br>\n";
+
+$array = [1, 2, 3];
+
+function myfunction(int $var): int
+{
+    return $var * 5;
+}
+
+print_r(array_map("myfunction", $array));
+
+echo "<br>\n";
+
+print_r(array_pad($array, - (count($array) + 1), "50"));
+echo "<br>\n";
+print_r(array_pad($array, count($array) + 1, "50"));
+
+echo "<br>\n";
+
+echo array_product($array);
+
+echo "<br>\n";
+
+array_pop($array);
+
+print_r($array);
+
+echo "<br>\n";
+
+sort($arr);
+print_r($arr);
+
+echo "<br>\n";
+
+echo $arr[array_rand($arr)];
+
+echo "<br>\n";
+
+array_push($arr, "anas");
+print_r($arr);
+
+echo "<br>\n";
+
+$arr = array_reverse($arr);
+print_r($arr);
+
+echo "<br>\n";
+
+print_r(array_sum(array(1, 2, 3, 4)));
+
+echo "<br>\n";
+
+$key = array_search("is nice", $combination);
+echo $key;
+
+echo "<br>\n";
+
+print_r(array_slice($arr, 1, 2));
+
+echo "<br>\n";
+
+$removed_elements = array_splice($arr, 0, 2);
+print_r($arr);
+echo "<br>\n";
+
+array_unshift($arr, "anas");
+print_r($arr);
+
+echo "<br>\n";
+
+$rachel = "nice";
+$harry = "stupid";
+$Hisham = "old";
+$people = compact("rachel", "harry", "Hisham");
+print_r($people);
+
+echo "<br>\n";
+extract($combination);
+
+echo "{$Andro} {$roger}";
+
+echo "<br>\n";
+
+
+
+echo "<br>\n";
+
+
+
+echo "<br>\n";
+
+echo "<br>\n";
+
+
+
+echo "<br>\n";
+
+
+
+
--- revision.php
+++ revision.php
@@ -1,38 +1,191 @@
 <?php
 
-// $dir = scandir(getcwd());
-// for ($i = 0; $i < count($dir); $i++)
-// {
-//     echo $dir[$i] . "<br>\n";
-// }
+$a = array(
+    [
+        "id" => "50",
+        "first_name" => "anas",
+        "last_name" => "wael"
+    ],
+    [
+        "id" => "51",
+        "first_name" => "peter",
+        "last_name" => "parker"
+    ],
+    [
+        "id" => "52",
+        "first_name" => "roger",
+        "last_name" => "paterson"
+    ]
+);
 
-// Another way 
+$Ids = array_column($a, "first_name", "id");
+print_r($Ids);
 
-// $dir = dir("C:\Users\anas\Documents");
+echo "<br>\n";
 
-// echo $dir->path . "<br>\n";
-// echo $dir->handle . "<br>\n";
+echo sizeof($a, 1);
 
-// while(TRUE)
-// {
-//     if (($file = $dir->read()) !== false)
-//     {
-//         echo $file . "<br>\n";
-//     }
-//     else
-//     {
-//         $dir->close();
-//         break;
-//     }
-// }
+echo "<br>\n";
 
-// Another way
+$arr1 = ["Peter", "roger", "Andro"];
+$arr2 = ["is nice", "is old", "is nice"];
 
-// $dir = opendir(getcwd());
+$combination = array_combine($arr1, $arr2);
 
-// while(($file = readdir($dir)) !== false )
-// {
-//     echo $file . "<br>\n";
-// }
+print_r($combination);
 
-// closedir($dir);
+echo "<br>\n";
+
+$array = array_change_key_case($combination, CASE_UPPER);
+print_r($array);
+
+echo "<br>\n";
+
+$array = array_merge($arr1, $arr2);
+
+$array = array_chunk($array, 2);
+
+print_r($array);
+
+echo "<br>\n";
+
+$arr2 = ["Peter", "roger", "Peter", "anas", "anas", "Peter"];
+
+print_r(array_count_values($arr2));
+
+echo "<br>\n";
+
+print_r(array_fill(0, 3, "anas is handsome"));
+
+echo "<br>\n";
+
+print_r(array_fill_keys($arr2, "is handsome"));
+
+echo "<br>\n";
+
+print_r(array_flip($a[0]));
+
+echo "<br>\n";
+
+print_r(array_intersect(["peter", "roger", "anas", "andro"], ["roger", "andro", "banana"]));
+
+echo "<br>\n";
+
+$array = array_filter(["Peter", "roger", "Peter", "anas", "anas", "Peter"], function ($var) { if ($var !== "Peter") { return true; } });
+
+print_r($array);
+
+echo "<br>\n";
+
+if (array_key_exists("roger", $combination))
+{
+    echo "Roger is in the array";
+}
+
+echo "<br>\n";
+
+$arr = array_keys($combination);
+
+foreach($arr as $value)
+{
+    echo $value . " ";
+}
+
+echo "<br>\n";
+
+$array = [1, 2, 3];
+
+function myfunction(int $var): int
+{
+    return $var * 5;
+}
+
+print_r(array_map("myfunction", $array));
+
+echo "<br>\n";
+
+print_r(array_pad($array, - (count($array) + 1), "50"));
+echo "<br>\n";
+print_r(array_pad($array, count($array) + 1, "50"));
+
+echo "<br>\n";
+
+echo array_product($array);
+
+echo "<br>\n";
+
+array_pop($array);
+
+print_r($array);
+
+echo "<br>\n";
+
+sort($arr);
+print_r($arr);
+
+echo "<br>\n";
+
+echo $arr[array_rand($arr)];
+
+echo "<br>\n";
+
+array_push($arr, "anas");
+print_r($arr);
+
+echo "<br>\n";
+
+$arr = array_reverse($arr);
+print_r($arr);
+
+echo "<br>\n";
+
+print_r(array_sum(array(1, 2, 3, 4)));
+
+echo "<br>\n";
+
+$key = array_search("is nice", $combination);
+echo $key;
+
+echo "<br>\n";
+
+print_r(array_slice($arr, 1, 2));
+
+echo "<br>\n";
+
+$removed_elements = array_splice($arr, 0, 2);
+print_r($arr);
+echo "<br>\n";
+
+array_unshift($arr, "anas");
+print_r($arr);
+
+echo "<br>\n";
+
+$rachel = "nice";
+$harry = "stupid";
+$Hisham = "old";
+$people = compact("rachel", "harry", "Hisham");
+print_r($people);
+
+echo "<br>\n";
+extract($combination);
+
+echo "{$Andro} {$roger}";
+
+echo "<br>\n";
+
+
+
+echo "<br>\n";
+
+
+
+echo "<br>\n";
+
+echo "<br>\n";
+
+
+
+echo "<br>\n";
+
+
